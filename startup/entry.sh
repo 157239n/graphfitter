4a018474269bd657dabca082dbf0e1e2b78362a6
Quang Ho <157239q@gmail.com>
2022-05-10 17:44:16 +0000

    Added drag and drop feature

--- startup/entry.sh
+++ startup/entry.sh
@@ -5,6 +5,6 @@ chown -R www-data:www-data /var/www/html
 /startup/runPhpFpm.sh
 cat /etc/environment | /startup/setupFpmEnv.sh
 
-python3 /startup/backend.py
+python3 /startup/backend.py 1>>/startup/backend.log 2>>/startup/backend.error.log
 
 tail -f /dev/null
